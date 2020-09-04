2861e0adb10e5ef68e758813d6b396e6fa05d8d0
Jozeevis <lars0795@gmail.com>
2020-09-05 00:20:08 +0200

    Fixed more 2fc formatting

--- logic/card-helper.js
+++ logic/card-helper.js
@@ -8,16 +8,7 @@ module.exports = {
 function generateCardMessage(card) {
     let cardName = card.name;
     logging.Log(`Sending ${cardName} to channel.`);
-
-    let cardCost = card.mana_cost
-      ? card.mana_cost.replace(new RegExp("[{}]", "g"), "")
-      : "";
-    let cardText = generateDescriptionText(card);
-
-    // Construct the discord message
-    let message = `**${cardName}** - ${cardCost}\n${cardText}`;
-
-    return message;
+    return generateDescriptionText(card);
 }
 
 // Generate the description of a card
@@ -27,42 +18,71 @@ function generateDescriptionText(card) {
     `**${card.power.replace(/\*/g, "\\*")}/${card.toughness.replace(/\*/g, "\\*")}**`;
 
   const description = [];
-  if (card.type_line) {
-    // bold type line
-    let type = `${card.printed_type_line || card.type_line}`;
-    type += ` (${_.capitalize(card.rarity)})`;
-    description.push(type);
+  if (!card.card_faces) {
+      let cardCost = card.mana_cost
+        ? card.mana_cost.replace(new RegExp("[{}]", "g"), "")
+        : "";
+      let nameLine = `**${card.name}**`
+      if (cardCost) {
+          nameLine +=  ` - ${cardCost}`;
+      }
+      description.push(nameLine);
+
+      if (card.type_line) {
+        // bold type line
+        let type = `${card.printed_type_line || card.type_line}`;
+        type += ` (${_.capitalize(card.rarity)})`;
+        description.push(type);
+      }
+
+      if (card.oracle_text) {
+        // reminder text in italics
+        const text = card.printed_text || card.oracle_text;
+        description.push(text.replace(/[()]/g, (m) => (m === "(" ? "_(" : ")_")));
+      }
+    
+      if (card.flavor_text) {
+        // flavor text in italics
+        description.push(`_${card.flavor_text}_`);
+      }
+    
+      if (card.loyalty) {
+        // bold loyalty
+        description.push(`**Loyalty: ${card.loyalty}**`);
+      }
+    
+      if (card.power) {
+        // bold P/T
+        description.push(ptToString(card));
+      }
+    
+        if (card.image_uris) {
+            description.push(getImageUrl(card.image_uris));
+        }
   }
-
-  if (card.oracle_text) {
-    // reminder text in italics
-    const text = card.printed_text || card.oracle_text;
-    description.push(text.replace(/[()]/g, (m) => (m === "(" ? "_(" : ")_")));
-  }
-
-  if (card.flavor_text) {
-    // flavor text in italics
-    description.push(`_${card.flavor_text}_`);
-  }
-
-  if (card.loyalty) {
-    // bold loyalty
-    description.push(`**Loyalty: ${card.loyalty}**`);
-  }
-
-  if (card.power) {
-    // bold P/T
-    description.push(ptToString(card));
-  }
-
-    if (card.image_uris) {
-        description.push(getImageUrl(card.image_uris));
-    }
-
-  if (card.card_faces) {
+  else {
     // split cards are special
+    let nameLine = `**${card.name}**`
+    nameLine += ` _(2-faced card)_`
+    description.push(nameLine);
+
     card.card_faces.forEach((face) => {
-      description.push(`**${face.type_line}**`);
+        let faceCost = face.mana_cost
+          ? face.mana_cost.replace(new RegExp("[{}]", "g"), "")
+          : "";
+          let nameLine = `**${face.name}**`
+          if (faceCost) {
+              nameLine +=  ` - ${faceCost}`;
+          }
+        description.push(nameLine);
+
+        if (face.type_line) {
+            // bold type line
+            let type = `${face.printed_type_line || face.type_line}`;
+            type += ` (${_.capitalize(card.rarity)})`;
+            description.push(type);
+          }
+
       if (face.oracle_text) {
         description.push(
           face.oracle_text.replace(/[()]/g, (m) => (m === "(" ? "_(" : ")_"))
@@ -71,7 +91,9 @@ function generateDescriptionText(card) {
       if (face.power) {
         description.push(ptToString(face));
       }
-      description.push(getImageUrl(face.image_uris));
+      if (face.image_uris) {
+        description.push(getImageUrl(face.image_uris));
+      }
       description.push("");
     });
   }
